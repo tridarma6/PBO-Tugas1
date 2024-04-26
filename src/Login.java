b1f823fcd64f9b3376198a7cabb27d4a5110db31
PutuRifki <puturifki0@gmail.com>
2024-04-26 09:46:25 +0800

    USERNAME DAN PASSWORD

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,11 @@
 import java.util.*;
 public class Login {
+    private static final String ADMIN_USERNAME = "admin";
+    private static final String ADMIN_PASSWORD = "a190605";
+    private static final String CUSTOMER_USERNAME = "customer";
+    private static final String CUSTOMER_PASSWORD = "c190605";
     public static void login(){
+
         Scanner scanner = new Scanner(System.in);
 
         Message.tampilanLogin();
@@ -14,7 +19,7 @@ public class Login {
             String username = scanner.nextLine();
             System.out.println("Masukkan password Anda:");
             String password = scanner.nextLine();
-        if (username.equals("adminUsername") && password.equals("adminPassword")) {
+        if (username.equals(ADMIN_USERNAME) && password.equals(ADMIN_PASSWORD)) {
             CLS.clearScreen();
             System.out.println("Login admin berhasil!");
         } else {
@@ -28,7 +33,7 @@ public class Login {
         String username = scanner.nextLine();
         System.out.println("Masukkan password Anda:");
         String password = scanner.nextLine();
-        if (username.equals("customerUsername") && password.equals("customerPassword")) {
+        if (username.equals(CUSTOMER_USERNAME) && password.equals(CUSTOMER_PASSWORD)) {
             CLS.clearScreen();
             System.out.println("Login customer berhasil!");
         } else {
